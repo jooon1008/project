fde5b7f2771817dc53e7c154c0ed60df0607e2a5
SAMSUNG\sec <kimjooon1008@gmail.com>
2018-03-17 21:46:52 +0900

    문제를 만드는 파트

--- calc.py
+++ calc.py
@@ -1,1 +1,14 @@
-
+import random
+def make_puetion():
+	x = random.randint(1,100)
+	y = random.randint(1,100)
+	giho = random.randint(1,3)
+	if giho == 1:
+		giho = '+'
+	elif giho == 2:
+		giho = '-'
+	elif giho == 3:
+		giho = '*'
+	quetion = str(x) + giho + str(y)
+	return quetion
+	
