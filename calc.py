9c9575ecefafc90ddd6144664ac6c0af02e75fd2
SAMSUNG\sec <kimjooon1008@gmail.com>
2018-03-17 22:12:30 +0900

    질문하기,대조하기

--- calc.py
+++ calc.py
@@ -1,5 +1,5 @@
 import random
-def make_puetion():
+def make_quetion():
 	x = random.randint(1,100)
 	y = random.randint(1,100)
 	giho = random.randint(1,3)
@@ -11,4 +11,22 @@ def make_puetion():
 		giho = '*'
 	quetion = str(x) + giho + str(y)
 	return quetion
+while_key = True
+while while_key:
+	print ('랜덤으로 나오는 문제들을 풀어주십시오.')
+	quetion = make_quetion()
+	r_a = eval(quetion)
+	print ('이 문제의 답을 입력해 주세요.\n' + quetion + ' = ?')
+	u_a = input()
+	if int(u_a) == r_a:
+		print("맞았습니다!")
+	else:
+		print("틀렸네요.")
+	if_q = input('더 하시겠습니까?(y/n)')
+	if if_q == 'n':
+		while_key = False
+	elif if_q != 'n''y':
+		print('알 수 없는 대답이네요.')
+			
 	
+			
